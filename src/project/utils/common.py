db13b3e6036d33854939f04671afac8e07710ba9
Aditya <yraditya895@gmail.com>
2025-09-13 12:38:29 +0530

    commons

--- src/project/utils/common.py
+++ src/project/utils/common.py
@@ -1,2 +1,24 @@
 import os
 from box.exceptions import BoxValueError
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+import yaml
+import joblib
+import json
+from ensure import ensure_annotations
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """
+    reads yaml file and returns
+    """
+    try:
+        with open(path_to_yaml, "r") as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+
